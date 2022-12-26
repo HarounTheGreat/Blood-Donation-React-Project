45bfaacc5412485c81a4dee3721d81041f948e80
HarounTheGreat <harounbouriga99@gmail.com>
2022-12-26 23:52:31 +0100

    React-Notes

--- test/T.js
+++ test/T.js
@@ -1,21 +1,26 @@
-
-showDate = (time) => {
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ]
-
-  const month = months[time.getMonth()].slice(0, 3)
-  const year = time.getFullYear()
-  const date = time.getDate()
-  return ` ${month} ${date}, ${year}`
+const UserCard = ({ user: { firstName, lastName, image } }) => (
+  <div className='user-card'>
+    <img src={image} alt={firstName} />
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
+  </div>
+)
+this.props.user.image
+this.props.user.firstName
+this.props.user.lastName
+class UserCard extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    <div className='user-card'>
+    <img src={this.props.user.image} alt={this.props.user.firstName} />
+    <h2>
+      {this.props.user.firstName}
+      {this.props.user.lastName}
+    </h2>
+  </div>
+  }
+}
